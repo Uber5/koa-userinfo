af60b03af2efc7d17d79233898e9ffbd0334dc9e
Chris Oloff <christian.oloff@gmail.com>
2017-09-01 17:16:02 +0200

    extract access token from header, optionally continue

--- src/__tests__/middleware-spec.js
+++ src/__tests__/middleware-spec.js
@@ -1,9 +1,43 @@
 import koaUserinfo from '../'
 
+const newCtx = () => ({
+  headers: {},
+  throw: jest.fn()
+})
+
 describe('the middleware', () => {
+
   describe('creating it', () => {
     it('fails without site', () => {
       expect(() => koaUserinfo({})).toThrow()
     })
   })
+
+  describe('using it', () => {
+
+    it('throws without or invalid authorization header', () => {
+
+      const middleware = koaUserinfo({ site: '123' })
+      const ctx = newCtx()
+      // expect(() => middleware(ctx)).toThrow()
+      middleware(ctx)
+      expect(ctx.throw.mock.calls.length).toBe(1)
+
+    })
+
+    it('does not throw if throwing disabled', async () => {
+      
+      const middleware = koaUserinfo({
+        site: '123',
+        allowMissingOrInvalidToken: true
+      })
+      const ctx = newCtx()
+      
+      return await new Promise((resolve, reject) => {
+        middleware(ctx, () => resolve())
+      })
+
+    })
+  })
+
 })
--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ type Options = {
   allowMissingOrInvalidToken?: boolean
 }
 
+const getAccessTokenFromHeader = ctx => {
+  const auth = ctx.headers.authorization
+  if (!auth) {
+    return undefined
+  }
+  const match = auth.match(/^Bearer (.+)$/)
+  return match && match[1]
+}
+
+
+
 export default (options: Options) => {
 
   if (!options.site) {
@@ -12,6 +23,12 @@ export default (options: Options) => {
   }
 
   return async (ctx, next) => {
+
+    const token = getAccessTokenFromHeader(ctx)
+    if (!token && !options.allowMissingOrInvalidToken) {
+      return ctx.throw(401, 'Unable to use / extract Bearer token')
+    }
+    
     console.log('do stuff...')
     await next()
   }
